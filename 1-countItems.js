5206872733efe75006ab1ea680808291cb5ae7ef
SaraMsabet <sarasabet9998@gmail.com>
2023-12-13 16:17:48 +0330

    1

--- 1-countItems.js
+++ 1-countItems.js
@@ -1,1 +1,9 @@
 //Count the number of <li> in the countItems.html file and show the results in console.
+let liCollection = document.querySelectorAll("li");
+let counter=0
+for (const element of liCollection) {
+    
+    console.log(element);
+    counter++
+}
+console.log(counter);

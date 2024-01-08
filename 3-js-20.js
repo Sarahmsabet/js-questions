bee990271a9613fd5a15a8513eaa60db1b7a6a71
SaraMsabet <sarasabet9998@gmail.com>
2024-01-09 02:09:47 +0330

    yadam raft

--- 3-js-20.js
+++ 3-js-20.js
@@ -1,3 +1,9 @@
 // ? why event bubbling is bad and how can we prevent it?
-
+let heading = document.createElement("h1");
+let body=document.querySelector("body")
+body.append(heading);
+heading.innerText = "log for third question";
 // ! Answers ======================================
+console.log(
+  "well imagin we have some nested element and each one of them has there own special event.\nif you click on one of there child thhe hole family would run\nhow can you stop it by'stoppropagetion'method or 'stopimidiatepropagation'"
+);
